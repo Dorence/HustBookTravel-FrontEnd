130278e7ae0b2d16d065b09256cfa31a00e99c71
Yingli Chen <1070116327@qq.com>
2019-09-16 23:54:53 +0800

    register and login

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,18 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import HomePage from '@/components/views/HomePage'
+import IndexRL from '@/components/views/IndexRL'
 
 export default new Router({
     routes: [{
         path: '/',
         name: 'HomePage',
         component: HomePage
-    }]
+    },
+    {
+        path:"/IndexRL",
+        name:'IndexRL',
+        component: IndexRL
+    }
+    ]
 })
