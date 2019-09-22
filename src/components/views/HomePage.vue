60cd097300612ca6735465fb0125db2f4129f682
izhuhaoran <2298600490@qq.com>
2019-09-22 22:13:08 +0800

    zhr

--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -28,26 +28,32 @@
     </el-row>
     <el-divider></el-divider>
     <el-row class="booktravel-notice booktravel-home-margin" :gutter="8">
-      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="6">
+      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="6" v-for="i in fdback" v-bind:key="i.id">
         <el-card shadow="hover">
-          <div slot="header" class="booktravel-notice-header">如果读者遗失了图书，图书馆会如何处理？</div>
-          <p>
-            书刊遗失，应及时报失。尽量购买原版本（ISBN号相同）图书抵还，要求图书基本完好，没有缺页，没有涂划，并交纳文献加工费每册10元。
-            <br />如购买不到原版图书，按以下规定赔偿：流通部门将待赔的图书书目信息统一发送到采编部，由采编部专人核定赔书金额。流通部门的工作人员根据采编部意见处理赔书业务。赔书的受理时间为5个工作日。
-            <br />办理完赔书手续后，图书馆将不再接纳读者又找回的原书。
-          </p>
+          <div slot="header" class="booktravel-notice-header">
+            <span>{{i.title}}</span>
+          </div>
+          <p class="booktravel-notice-p">{{i.body}}</p>
         </el-card>
       </el-col>
-      <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="6">
-        <el-card shadow="hover">
-          <div slot="header" class="booktravel-notice-header">如果读者遗失了图书，图书馆会如何处理？</div>
-          <p>
-            书刊遗失，应及时报失。尽量购买原版本（ISBN号相同）图书抵还，要求图书基本完好，没有缺页，没有涂划，并交纳文献加工费每册10元。
-            <br />如购买不到原版图书，按以下规定赔偿：流通部门将待赔的图书书目信息统一发送到采编部，由采编部专人核定赔书金额。流通部门的工作人员根据采编部意见处理赔书业务。赔书的受理时间为5个工作日。
-            <br />办理完赔书手续后，图书馆将不再接纳读者又找回的原书。
-          </p>
-        </el-card>
+    </el-row>
+    <el-row class="notice">
+      <el-col :span="16">
+        <h2>如果遇到紧急特殊情况怎么办？</h2>
+        <p class="notice-body">
+          大家可以在线下找到我们站点勤工岗的同学进行咨询，
+          <br />同时也可按照下面的联系方式，联系我们主办方。
+          <br />张鸿鹄——电话：17786284532&ensp;QQ：634865427
+          <br />如果有什么紧急问题也可以加入我们的活动群（点击右侧图标）
+          <el-popover placement="right" width="400" trigger="click">
+            <img :src="qrcodeURL" class="my-qrcode-image" />
+            <img :src="ImgURL" slot="reference" class="my-scan-image" />
+          </el-popover>
+        </p>
       </el-col>
+      <!-- <el-col :span="8">
+        <el-image :src="url" :fit="fit"></el-image>
+      </el-col>-->
     </el-row>
   </div>
 </template>
@@ -55,7 +61,48 @@
 export default {
   name: "HomePage",
   data() {
-    return {};
+    return {
+      qrcodeURL: "../../../static/qrcode.jpg",
+      ImgURL: "../../../static/scan.png",
+      fdback: [
+        {
+          id: 0,
+          title: "新同学怎么借书？",
+          body:
+            "同学可以在我们勤工岗的同学的帮助下在网页完成注册，输入自己的基本信息,之后可以登陆进入我们的主页，在左上角点击主页旁边，点击借书，即可在勤工岗同学处进行图书借阅,同学在注册的时候需要如实填写自己的基本信息。"
+        },
+        {
+          id: 1,
+          title: "在哪里能借到图书？在哪里能还图书呢？",
+          body:
+            "本次图书漂流活动设有四个站点，分别是西十二、西五、东九、东十二四处的勤工岗，在四个站点处均可还书,但是借书需要在主页上查询图书所在的站点，去往对于位置站点进行借书。"
+        },
+        {
+          id: 2,
+          title: "图书在借阅的过程中损坏、污损怎么办？",
+          body:
+            "请同学们在借阅的过程中仔细保管好自己的图书，在还书的时候会有勤工岗同学进行查看图书的完整度。大家可以在书中书写自己的看法（留小纸条等待一个有缘的拾荒者）但是不得恶意损坏、撕毁、污损书籍,我们会根据图书的情况酌情处理，会体谅图书有误损的情形，但是绝对不会漏掉恶意损坏的情况。"
+        },
+        {
+          id: 3,
+          title: "想发表评论或分享我喜爱的书籍怎么办？",
+          body:
+            "我们的网站设有线上评论区，可以供大家在线上评论交流，也可以让大家找到与你志同道合的那一拾荒者。让你我在书海里一同遨游，向同一个方向进发。"
+        },
+        {
+          id: 4,
+          title: "借阅有时间限制吗？想继续借阅怎么办？",
+          body:
+            "本次活动借阅时间为7天时间\n如果大家出现未读完的情况,可以到各个站点的勤工岗同学处或线上申请加时借阅，加时借阅为期7天，最多每人加时1次。"
+        },
+        {
+          id: 5,
+          title: "作为第二个子活动和之后的子活动有关联吗？",
+          body:
+            "我们会在评论中选择喜爱度和讨论度最高的书籍中选出一部分，会在第三个子活动解读会举办的时候请相关专家来进行深入解读。大家如果有特别想推荐的书籍也可以联系我们主办方，同时我们会对推荐热门书籍的同学给予相应的一些奖励。"
+        }
+      ]
+    };
   },
   methods: {}
 };
@@ -100,12 +147,13 @@ blockquote small {
 
 .booktravel-intro blockquote {
   width: 420px;
-  font-family: 	SimSun;
-  font-size: 16px
+  font-family: SimSun;
+  font-size: 16px;
 }
 
 .booktravel-notice .el-card {
   margin-bottom: 1rem;
+  height: 350px;
 }
 
 .booktravel-notice-header {
@@ -116,5 +164,19 @@ blockquote small {
 .booktravel-home-margin {
   margin-left: 8px !important;
   margin-right: 8px !important;
+}
+
+.booktravel-notice-p {
+  white-space: pre-line;
+}
+.notice {
+  background-color: #67c23a;
+}
+.notice h2,
+.notice p {
+  color: #fafafa;
+}
+.my-scan-image {
+  height: 25px;
 }
 </style>
